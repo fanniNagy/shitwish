8d45f47ee86e05c67bc3f1bc2de84da72cd3d222
Verhóczki Márton <verhoczki.marci@gmail.com>
2020-01-07 15:28:11 +0100

    Extend ProductController

--- product_service/src/main/java/codecool/product_service/controller/ProductController.java
+++ product_service/src/main/java/codecool/product_service/controller/ProductController.java
@@ -1,10 +1,13 @@
 package codecool.product_service.controller;
 
 import codecool.product_service.model.Product;
-import codecool.product_service.model.request.RequestProduct;
+import codecool.product_service.model.SellerDetail;
+import codecool.product_service.model.request_response.ProductWithSellerDetail;
 import codecool.product_service.service.ProductService;
+import codecool.product_service.service.SellerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,27 +21,21 @@ public class ProductController {
 
     ProductService productService;
 
+    SellerService sellerService;
+
     @GetMapping
     List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
 
     @GetMapping("/{id}")
-    Product getProductById(@PathVariable("id") Long id) {
-        return productService.getProductById(id);
+    ProductWithSellerDetail getProductById(@PathVariable("id") Long id) {
+        return productService.getProductWithSellerDetailById(id);
     }
 
     @PostMapping
-    Product addProduct(@RequestBody RequestProduct requestProduct) {
-        return productService.addProduct(
-                Product.builder()
-                .name(requestProduct.getName())
-                .condition(requestProduct.getCondition())
-                .description(requestProduct.getDescription())
-                .pictureUrl(requestProduct.getPictureUrl())
-                .price(requestProduct.getPrice())
-                .build()
-        );
+    Product addProduct(@RequestBody ProductWithSellerDetail productWithSellerDetail) throws JSONException {
+        return productService.addProduct(productWithSellerDetail);
     }
 
     @DeleteMapping("/{id}")
