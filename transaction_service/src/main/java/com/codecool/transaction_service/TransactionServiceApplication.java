78f2b440d67feebce866342ae9dfdad04152ed1c
Alex <alexalpek@gmail.com>
2020-01-07 14:13:21 +0100

    commiting before rebase of dev to current since eureaka server needed and zuul.

--- transaction_service/src/main/java/com/codecool/transaction_service/TransactionServiceApplication.java
+++ transaction_service/src/main/java/com/codecool/transaction_service/TransactionServiceApplication.java
@@ -1,13 +1,25 @@
 package com.codecool.transaction_service;
 
+import com.codecool.transaction_service.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
+//TODO ENABLE EU-RÉKA here and app.properties
 public class TransactionServiceApplication {
+
+    @Autowired
+    private TransactionRepository repository;
 
     public static void main(String[] args) {
         SpringApplication.run(TransactionServiceApplication.class, args);
     }
 
+    @PostConstruct
+    public void initializeData(){
+
+    }
 }
---
+++ transaction_service/src/main/java/com/codecool/transaction_service/service/TransactionService.java
@@ -0,0 +1,7 @@
+package com.codecool.transaction_service.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService {
+}
