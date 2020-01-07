1dd06f85a8c922580d9ada3a5494de6d5894b227
fanniNagy <nagy.fanni.5@gmail.com>
2020-01-07 14:00:21 +0100

    add Seller model with repository

---
+++ seller_service/src/main/java/com/codecool/seller_service/repository/SellerRepository.java
@@ -0,0 +1,17 @@
+package com.codecool.seller_service.repository;
+
+import com.codecool.seller_service.model.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+    Seller findSellerById(Long id);
+
+    @Query(value = "DELETE FROM Seller where Seller.id = :id")
+    void deleteSellerById(@Param("id") long id);
+
+
+
+}
