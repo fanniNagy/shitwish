a88e66a948b010abfa52f97644e5a7a876c39d3f
Alex <alexalpek@gmail.com>
2020-01-07 14:13:21 +0100

    commiting before rebase of dev to current since eureaka server needed and zuul.

---
+++ transaction_service/src/main/java/com/codecool/transaction_service/controller/TransactionController.java
@@ -0,0 +1,13 @@
+package com.codecool.transaction_service.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transaction")
+@CrossOrigin
+public class TransactionController {
+
+
+}
